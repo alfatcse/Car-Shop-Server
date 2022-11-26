71aeb76846ab64fd1b12b0fc1587cf17476d8310
Alfat <alfat.cse.21@gmail.com>
2022-11-26 17:01:33 +0100

    shorting done

--- index.js
+++ index.js
@@ -53,7 +53,9 @@ async function run() {
         })
         app.get('/service', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const order=req.query.order;
+            console.log(order);
+            const cursor = serviceCollection.find(query).sort({price:order});
             const services = await cursor.toArray();
             res.send(services);
         })
@@ -64,7 +66,7 @@ async function run() {
             res.send(service);
         })
         //order api
-        app.post('/orders',async (req, res) => {
+        app.post('/orders',verifyJWT,async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
@@ -87,13 +89,13 @@ async function run() {
             const orders = await cursor.toArray();
             res.send(orders);
         })
-        app.delete('/orders/:id', async (req, res) => {
+        app.delete('/orders/:id', verifyJWT,async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result=await orderCollection.deleteOne(query);
             res.send(result);
         })
-        app.patch('/orders/:id',async(req,res)=>{
+        app.patch('/orders/:id',verifyJWT,async(req,res)=>{
             const id=req.params.id;
             const status=req.body.status;
             const query={_id:ObjectId(id)};
